9e8510af7515ff2c5fc19c4c52e8eee34092005d
ArcherHuang <mmosconii@gmail.com>
2019-10-10 08:49:12 +0800

    Fix getOrders 判斷是否此查詢的訂單存在 & 加上 try-catch

--- services/admin/orderService.js
+++ services/admin/orderService.js
@@ -4,18 +4,24 @@ const Order = db.Order
 const orderService = {
 
   getOrders: (req, res, callback) => {
-    Order.findAll().then(orders => {
-
-      if (req.params.order_id) {
-        Order.findByPk(req.params.order_id)
-          .then((order) => {
-            return callback({ status: 'success', message: '取得特定 Order 資料', content: order })
-          })
-      } else {
-        return callback({ status: 'success', message: '取得 Order 所有清單', content: orders })
-      }
-
-    })
+    try {
+      Order.findAll().then(orders => {
+        if (req.params.order_id) {
+          Order.findByPk(req.params.order_id)
+            .then((order) => {
+              if (order) {
+                return callback({ status: 'success', message: '取得特定 Order 資料', content: order })
+              } else {
+                callback({ status: 'fail', message: '查無此 Order 存在' })
+              }
+            })
+        } else {
+          return callback({ status: 'success', message: '取得 Order 所有清單', content: orders })
+        }
+      })
+    } catch (error) {
+      return res.status(500).json({ status: 'error', message: error })
+    }
   },
 
   // 訂單刪除 dataStatus = 0
@@ -24,70 +30,86 @@ const orderService = {
 
   // 訂單取消：1 變 2
   cancelOrder: (req, res, callback) => {
-    Order.findByPk(req.params.order_id)
-      .then((order) => {
-        if (order) {
-          order.update({
-            dataStatus: 2
-          })
-            .then((order) => {
-              callback({ status: 'success', message: 'Order 已取消訂單成功' })
+    try {
+      Order.findByPk(req.params.order_id)
+        .then((order) => {
+          if (order) {
+            order.update({
+              dataStatus: 2
             })
-        } else {
-          callback({ status: 'fail', message: '查無此 Order 存在' })
-        }
-      })
+              .then((order) => {
+                callback({ status: 'success', message: 'Order 已取消訂單成功' })
+              })
+          } else {
+            callback({ status: 'fail', message: '查無此 Order 存在' })
+          }
+        })
+    } catch (error) {
+      return res.status(500).json({ status: 'error', message: error })
+    }
   },
 
   // 訂單恢復：2 變 1
   resumeOrder: (req, res, callback) => {
-    Order.findByPk(req.params.order_id)
-      .then((order) => {
-        if (order) {
-          order.update({
-            dataStatus: 1
-          })
-            .then((order) => {
-              callback({ status: 'success', message: 'Order 已恢復訂單成功' })
+    try {
+      Order.findByPk(req.params.order_id)
+        .then((order) => {
+          if (order) {
+            order.update({
+              dataStatus: 1
             })
-        } else {
-          callback({ status: 'fail', message: '查無此 Order 存在' })
-        }
-      })
+              .then((order) => {
+                callback({ status: 'success', message: 'Order 已恢復訂單成功' })
+              })
+          } else {
+            callback({ status: 'fail', message: '查無此 Order 存在' })
+          }
+        })
+    } catch (error) {
+      return res.status(500).json({ status: 'error', message: error })
+    }
   },
 
   // 出貨 shippingStatus = 1
   shippedOrder: (req, res, callback) => {
-    Order.findByPk(req.params.order_id)
-      .then((order) => {
-        if (order) {
-          order.update({
-            shippingStatus: 1
-          })
-            .then((order) => {
-              callback({ status: 'success', message: 'Order 已出貨成功' })
+    try {
+      Order.findByPk(req.params.order_id)
+        .then((order) => {
+          if (order) {
+            order.update({
+              shippingStatus: 1
             })
-        } else {
-          callback({ status: 'fail', message: '查無此 Order 存在' })
-        }
-      })
+              .then((order) => {
+                callback({ status: 'success', message: 'Order 已出貨成功' })
+              })
+          } else {
+            callback({ status: 'fail', message: '查無此 Order 存在' })
+          }
+        })
+    } catch (error) {
+      return res.status(500).json({ status: 'error', message: error })
+    }
   },
 
   // 取消出貨 shippingStatus = 2
   unshippedOrder: (req, res, callback) => {
-    Order.findByPk(req.params.order_id)
-      .then((order) => {
-        if (order) {
-          order.update({
-            shippingStatus: 2
-          })
-            .then((order) => {
-              callback({ status: 'success', message: 'Order 已取消出貨成功' })
+    try {
+      Order.findByPk(req.params.order_id)
+        .then((order) => {
+          if (order) {
+            order.update({
+              shippingStatus: 2
             })
-        } else {
-          callback({ status: 'fail', message: '查無此 Order 存在' })
-        }
-      })
+              .then((order) => {
+                callback({ status: 'success', message: 'Order 已取消出貨成功' })
+              })
+          } else {
+            callback({ status: 'fail', message: '查無此 Order 存在' })
+          }
+        })
+    } catch (error) {
+      return res.status(500).json({ status: 'error', message: error })
+    }
   },
 
   // 待出貨 shippingStatus = 0
