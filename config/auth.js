493b1e0ba446e73c5a2748a28eb184d1af36d88d
jacs <wbruce0110@gmail.com>
2019-10-16 14:58:39 +0800

    create isAuthUser function

--- config/auth.js
+++ config/auth.js
@@ -2,19 +2,32 @@ const helper = require('../_helpers')
 const passport = require('./passport')
 
 module.exports = {
+
   ensureAuthenticated: (req, res, next) => {
     if (helper.ensureAuthenticated()) {
       return next()
     }
     return passport.authenticate('jwt', { session: false })(req, res, next)
   },
+
   getUser: (req, res, next) => {
     req.user = helper.getUser(req)
     next()
   },
+
   isAuthAdmin: (req, res, next) => {
     if (!req.user) return res.json({ status: 'error', message: 'Permission denied' })
     if (req.user.role !== 'admin') return res.json({ status: 'error', message: 'Permission denied' })
     next()
+  },
+
+  isAuthUser: (req, res, next) => {
+    if (!req.user) return res.json({ status: 'error', message: 'Permission denied' })
+    if (req.user.role == 'admin' || req.user.role == 'user') {
+      next()
+    } else {
+      return res.json({ status: 'error', message: 'Permission denied' })
+    }
+
   }
 }
