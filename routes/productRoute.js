c923a0602845e71e791aa4fa832a30a4c6a321cc
jacs <wbruce0110@gmail.com>
2019-10-16 15:04:54 +0800

    update product service, routes, controller

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const adminProductController = require('../controllers/admin/productController')
 const productController = require('../controllers/productController')
 
-// Admin 操作
-router.get('/', adminProductController.getProducts)
-router.get('/:product_id', adminProductController.getProduct)
-
-// 一般 User 操作
+router.get('/', productController.getProducts)
+router.get('/:product_id', productController.getProduct)
 router.post('/:product_id/like', productController.likeProduct)
 router.post('/:product_id/unlike', productController.unlikeProduct)
 
--- services/productService.js
+++ services/productService.js
@@ -4,9 +4,89 @@ const validator = require('validator')
 const moment = require('moment')
 
 const db = require('../models')
-const { User, Product, Like } = db
+const { Carousel, Category, User, Product, Like } = db
 
 const productService = {
+
+  getProducts: async (req, res, callback) => {
+
+    let carousels = await Carousel.findAll({
+      where: {
+        dataStatus: 1
+      }
+    })
+    let categories = await Category.findAll({
+      where: {
+        dataStatus: 1
+      }
+    })
+    let currentUser
+
+    if (req.session.user) {
+      currentUser = await User.findByPk(req.session.user.id)
+    } else {
+      currentUser = []
+    }
+
+    if (req.query.category_id && req.query.keyword) {
+      const keyword = req.query.keyword ? req.query.keyword : null
+      return Product.findAll({
+        include: [Category],
+        where: {
+          dataStatus: 1,
+          CategoryId: req.query.category_id ? req.query.category_id : null,
+          [Op.or]: [
+            { name: { [Op.like]: '%' + keyword + '%' } },
+            { description: { [Op.like]: '%' + keyword + '%' } }
+          ]
+        },
+        order: [['updatedAt', 'DESC']]
+      }).then(products => {
+        if (products.length !== 0) {
+          return callback({ status: 'success', message: '取得搜尋產品清單成功', content: products, carousels: carousels, categories: categories, currentUser: currentUser })
+        } else {
+          return Product.findAll({ include: [Category] }).then(products => {
+            return callback({ status: 'success', message: '該搜尋沒有產品，取得所有產品清單成功', content: products, carousels: carousels, categories: categories, currentUser: currentUser })
+          })
+        }
+      })
+
+    } else if (req.query.category_id || req.query.keyword) {
+      const keyword = req.query.keyword ? req.query.keyword : null
+      return Product.findAll({
+        include: [Category],
+        where: {
+          dataStatus: 1,
+          [Op.or]: [
+            { CategoryId: req.query.category_id ? req.query.category_id : null, },
+            { name: { [Op.like]: '%' + keyword + '%' } },
+            { description: { [Op.like]: '%' + keyword + '%' } }
+          ]
+        },
+        order: [['updatedAt', 'DESC']]
+      }).then(products => {
+        if (products.length !== 0) {
+          return callback({ status: 'success', message: '取得搜尋產品清單成功', content: products, carousels: carousels, categories: categories, currentUser: currentUser })
+        } else {
+          return Product.findAll({ include: [Category] }).then(products => {
+            return callback({ status: 'success', message: '該搜尋沒有產品，取得所有產品清單成功', content: products, carousels: carousels, categories: categories, currentUser: currentUser })
+          })
+        }
+      })
+    } else {
+      return Product.findAll({ include: [Category] }).then(products => {
+        return callback({ status: 'success', message: '取得所有產品清單成功', content: products, carousels: carousels, categories: categories, currentUser: currentUser })
+      })
+    }
+  },
+
+  getProduct: (req, res, callback) => {
+
+    return Product.findByPk(req.params.product_id, { include: [Category] }).then(product => {
+      return callback({ status: 'success', message: '取得特定產品成功', content: product })
+    })
+
+  },
 
   likeProduct: async (req, res, callback) => {
 
