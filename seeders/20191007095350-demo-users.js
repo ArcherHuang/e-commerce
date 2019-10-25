7190c9e072754f47591dfd1be4e9a6e5f3c425d5
jacs <wbruce0110@gmail.com>
2019-10-25 16:25:17 +0800

    update seed data

--- seeders/20191007095350-demo-users.js
+++ seeders/20191007095350-demo-users.js
@@ -1,6 +1,8 @@
 'use strict'
 const bcrypt = require('bcrypt-nodejs')
 const faker = require('faker')
+const db = require('../models')
+const { User, Product, Order, Coupon, CouponDistribution, Category } = db
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
@@ -15,6 +17,19 @@ module.exports = {
           phone: faker.phone.phoneNumberFormat(),
           address: faker.address.streetAddress(),
           birthday: faker.date.past(),
+          isValid: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: 'user66@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: 'admin',
+          name: 'user66',
+          phone: faker.phone.phoneNumberFormat(),
+          address: faker.address.streetAddress(),
+          birthday: faker.date.past(),
+          isValid: true,
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -26,6 +41,7 @@ module.exports = {
           phone: faker.phone.phoneNumberFormat(),
           address: faker.address.streetAddress(),
           birthday: faker.date.past(),
+          isValid: true,
           createdAt: new Date(),
           updatedAt: new Date()
         },
@@ -37,9 +53,46 @@ module.exports = {
           phone: faker.phone.phoneNumberFormat(),
           address: faker.address.streetAddress(),
           birthday: faker.date.past(),
+          isValid: true,
           createdAt: new Date(),
           updatedAt: new Date()
-        }
+        },
+        {
+          email: 'user3@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: 'user',
+          name: 'user3',
+          phone: faker.phone.phoneNumberFormat(),
+          address: faker.address.streetAddress(),
+          birthday: faker.date.past(),
+          isValid: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: 'user4@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: 'user',
+          name: 'user4',
+          phone: faker.phone.phoneNumberFormat(),
+          address: faker.address.streetAddress(),
+          birthday: faker.date.past(),
+          isValid: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          email: 'user5@example.com',
+          password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+          role: 'user',
+          name: 'user5',
+          phone: faker.phone.phoneNumberFormat(),
+          address: faker.address.streetAddress(),
+          birthday: faker.date.past(),
+          isValid: true,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
       ],
       {}
     )
@@ -86,6 +139,15 @@ module.exports = {
       {}
     )
 
+    // Get all new category ids
+    let categories = await Category.findAll().then(categories => {
+      let results = []
+      for (let i = 0; i < categories.length; i++) {
+        results.push(categories[i].id)
+      }
+      return results
+    })
+
     await queryInterface.bulkInsert(
       'Products',
       Array.from({ length: 100 }).map((d) => ({
@@ -121,8 +183,8 @@ module.exports = {
           max: 20000
         }),
         CategoryId: faker.random.number({
-          min: 1,
-          max: 5
+          min: categories[0],
+          max: categories[categories.length - 1]
         }),
         dataStatus: 1,
         createdAt: new Date(),
@@ -144,7 +206,129 @@ module.exports = {
           min: 1,
           max: 10
         }),
+        amount: 0,
         expireDate: faker.date.future(1),
+        dataStatus: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    await queryInterface.bulkInsert(
+      'Coupons',
+      Array.from({ length: 10 }).map((d) => ({
+        name: faker.lorem.word(),
+        sn: faker.random.alphaNumeric(8),
+        discount: 0,
+        numberOfLimitation: faker.random.number({
+          min: 1,
+          max: 10
+        }),
+        amount: faker.random.number({
+          min: 10,
+          max: 200
+        }),
+        expireDate: faker.date.future(1),
+        dataStatus: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    // Get all new user ids
+    let users = await User.findAll().then(users => {
+      let results = []
+      for (let i = 0; i < users.length; i++) {
+        results.push(users[i].id)
+      }
+      return results
+    })
+
+    // Get all new coupon ids
+    let coupons = await Coupon.findAll().then(coupons => {
+      let results = []
+      for (let i = 0; i < coupons.length; i++) {
+        results.push(coupons[i].id)
+      }
+      return results
+    })
+
+    await queryInterface.bulkInsert(
+      'CouponDistributions',
+      Array.from({ length: 20 }).map((d) => ({
+        CouponId: faker.random.number({
+          min: coupons[0],
+          max: coupons[coupons.length - 1]
+        }),
+        UserId: faker.random.number({
+          min: users[0],
+          max: users[users.length - 1]
+        }),
+        usageStatus: 1,
+        dataStatus: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    // Get all new products ids
+    let products = await Product.findAll().then(products => {
+      let results = []
+      for (let i = 0; i < products.length; i++) {
+        results.push(products[i].id)
+      }
+      return results
+    })
+
+    await queryInterface.bulkInsert(
+      'PageViews',
+      Array.from({ length: products.length }).map((d) => ({
+        viewCount: faker.random.number({
+          min: 1,
+          max: 100
+        }),
+        UserId: faker.random.number({
+          min: users[0],
+          max: users[users.length - 1]
+        }),
+        ProductId: faker.random.number({
+          min: products[0],
+          max: products[products.length - 1]
+        }),
+        dataStatus: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    await queryInterface.bulkInsert(
+      'Likes',
+      Array.from({ length: 100 }).map((d) => ({
+        UserId: faker.random.number({
+          min: users[0],
+          max: users[users.length - 1]
+        }),
+        ProductId: faker.random.number({
+          min: products[0],
+          max: products[products.length - 1]
+        }),
+        dataStatus: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      })),
+      {}
+    )
+
+    await queryInterface.bulkInsert(
+      'Carousels',
+      Array.from({ length: 10 }).map((d) => ({
+        name: faker.lorem.word(),
+        description: faker.lorem.sentence(),
+        image: faker.image.business(),
         dataStatus: 1,
         createdAt: new Date(),
         updatedAt: new Date()
@@ -158,5 +342,8 @@ module.exports = {
     await queryInterface.bulkDelete('Products', null, {})
     await queryInterface.bulkDelete('Coupons', null, {})
     await queryInterface.bulkDelete('Categories', null, {})
+    await queryInterface.bulkDelete('Carousels', null, {})
+    await queryInterface.bulkDelete('Likes', null, {})
+    await queryInterface.bulkDelete('PageViews', null, {})
   }
 }
