d7e3fa964e84eb5665cb6325ee1ef77c7eb4a167
Alvis Hsu <junchoon14@gmail.com>
2019-11-01 11:12:47 +0800

    Fix [be]get-/shop, fix #177

--- routes/hbs/productRoute.js
+++ routes/hbs/productRoute.js
@@ -6,5 +6,6 @@ const { ensureAuthenticated, isAuthUser, getUser } = require('../../config/auth'
 const productController = require('../../controllers/hbs/productController')
 
 router.get('/products', productController.getProducts)
+router.get('/shop', productController.getShop)
 
 module.exports = router
