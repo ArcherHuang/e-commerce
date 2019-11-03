dd7524ef067a27d59a6c5f19a465344a77114dfb
jacs <wbruce0110@gmail.com>
2019-11-03 11:49:56 +0800

    add on try/catch in productController

--- controllers/hbs/productController.js
+++ controllers/hbs/productController.js
@@ -4,53 +4,65 @@ const productController = {
 
   getProducts: (req, res) => {
     productService.getProducts(req, res, (data) => {
-      if (data['status'] === 'success') {
-        req.flash('success_messages', data['message'])
-        console.log(`1_req_user_req_user_req_user_req_user: ${req.user}`)
-        const num = 12
-        const range = data.content.length - num
-        const startNum = Math.floor(Math.random() * range) + 1
-        const randomProducts = data.content.slice(startNum, startNum + num)
+      try {
+        if (data['status'] === 'success') {
+          req.flash('success_messages', data['message'])
+          const num = 12
+          const range = data.content.length - num
+          const startNum = Math.floor(Math.random() * range) + 1
+          const randomProducts = data.content.slice(startNum, startNum + num)
 
-        return res.render('index', {
-          products: data.content, 
-          randomProducts: randomProducts,
-          carousels: data.carousels, 
-          categories: data.categories
-        })
-      } else {
-        return req.flash('error_messages', data['message'])
+          return res.render('index', {
+            products: data.content,
+            randomProducts: randomProducts,
+            carousels: data.carousels,
+            categories: data.categories
+          })
+        } else {
+          return req.flash('error_messages', data['message'])
+        }
+      }
+      catch (err) {
+        console.log(`Err: ${err}`)
+        res.redirect('back')
       }
     })
   },
 
   getShop: (req, res) => {
     productService.getProducts(req, res, (data) => {
-      if (data['status'] === 'success') {
-        req.flash('success_messages', data['message'])
-        console.log('----------------req.user----------------------',req.user)
-
-        return res.render('shop', {
-          products: data.content
-        })
-      } else {
-        return req.flash('error_messages', data['message'])
+      try {
+        if (data['status'] === 'success') {
+          req.flash('success_messages', data['message'])
+          return res.render('shop', {
+            products: data.content
+          })
+        } else {
+          return req.flash('error_messages', data['message'])
+        }
+      }
+      catch (err) {
+        console.log(`Err: ${err}`)
+        res.redirect('back')
       }
     })
   },
 
   getProduct: (req, res) => {
     productService.getProduct(req, res, (data) => {
-      if (data['status'] === 'success') {
-        req.flash('success_messages', data['message'])
-        console.log(req.user)
-        console.log('123')
-
-        return res.render('productDetail', {
-          product: data.content
-        })
-      } else {
-        return req.flash('error_messages', data['message'])
+      try {
+        if (data['status'] === 'success') {
+          req.flash('success_messages', data['message'])
+          return res.render('productDetail', {
+            product: data.content
+          })
+        } else {
+          return req.flash('error_messages', data['message'])
+        }
+      }
+      catch (err) {
+        console.log(`Err: ${err}`)
+        res.redirect('back')
       }
     })
   },
