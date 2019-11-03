efe2c95a4e59da6f7fdd52e9f1553552af69fd30
jacs <wbruce0110@gmail.com>
2019-11-03 11:45:38 +0800

    將所有未設定的 routes 導回 /products/main

--- routes/routes.js
+++ routes/routes.js
@@ -8,10 +8,13 @@ const accountRoute = require('./hbs/accountRoute')
 const cartRoute = require('./hbs/cartRoute')
 const productRoute = require('./hbs/productRoute')
 
-router.use('/', productRoute)
 router.use('/accounts', accountRoute)
 router.use('/carts', cartRoute)
 router.use('/products', productRoute)
+
+// 將其他 routes 導回首頁
+router.get('/', (req, res) => res.redirect('/products/main'))
+router.get('/:params', (req, res) => res.redirect('/products/main'))
 
 module.exports = router
 
