6e645ce1d79d4331f83a79cdd85d1b177e31b0ab
jacs <wbruce0110@gmail.com>
2019-11-03 16:56:46 +0800

    完成藍新金流付款頁面

---
+++ controllers/hbs/orderController.js
@@ -0,0 +1,57 @@
+const orderService = require('../../services/orderService.js')
+const userService = require('../../services/userService.js')
+
+const orderController = {
+
+  getNewebPayment: async (req, res) => {
+    try {
+      await orderService.getPayment(req, res, (data) => {
+        let paymentInfo
+        let order
+        if (data['status'] == 'success') {
+          paymentInfo = {
+            id: data.content['MerchantID'],
+            info: data.content['TradeInfo'],
+            sha: data.content['TradeSha'],
+            payGateWay: data.content['PayGateWay'],
+            version: data.content['Version'],
+            orderNo: data.content['MerchantOrderNo'],
+            order: data['order']
+          }
+          order = paymentInfo.order
+          res.render('ordersNewebPayment', { paymentInfo: paymentInfo, order: order })
+        } else {
+          req.flash('error_messages', '取得藍新金流付款資訊失敗')
+          res.redirect('back')
+        }
+      })
+    }
+    catch (err) {
+      console.log(`Err: ${err}`)
+      res.redirect('back')
+    }
+  },
+
+  newebpayCallback: async (req, res) => {
+    await orderService.newebpayCallback(req, res, (data) => {
+      try {
+        if (data['status'] == 'success') {
+          req.flash('success_messages', '付款成功！')
+          res.redirect('/orders/payment')
+        } else {
+          req.flash('error_messages', '付款失敗，請重新付款')
+          res.redirect('/accounts/orders')
+        }
+      }
+      catch (err) {
+        console.log(`Err: ${err}`)
+        res.redirect('/accounts/orders')
+      }
+    })
+  },
+
+  getStripePayment: (req, res) => {
+  }
+}
+
+module.exports = orderController
