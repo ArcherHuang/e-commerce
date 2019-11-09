c19a176a665b0b8c1fb4585fc460b6919e703bc6
jacs <wbruce0110@gmail.com>
2019-11-09 10:25:58 +0800

    add try/catch in admin/productService

--- services/admin/userService.js
+++ services/admin/userService.js
@@ -4,36 +4,47 @@ const User = db.User
 const adminService = {
 
   editUsers: (req, res, callback) => {
-    User.findAll().then(users => {
-      return callback({ status: 'success', message: '取得使用者清單成功', content: users })
-    })
+    try {
+      User.findAll().then(users => {
+        return callback({ status: 'success', message: '取得使用者清單成功', content: users })
+      })
+    }
+    catch (err) {
+      console.log(`Err: ${err}`)
+      return callback({ status: 'error', message: '取得使用者清單失敗' })
+    }
   },
 
   putUsers: (req, res, callback) => {
-    return User.findByPk(req.params.user_id)
-      .then(user => {
-        const {
-          role
-        } = user
+    try {
+      return User.findByPk(req.params.user_id)
+        .then(user => {
+          const {
+            role
+          } = user
 
-        if (role) {
-          user.update({
-            role: null
-          })
-            .then(user => {
-              return callback({ status: 'success', message: `${user.email} 帳號已設定為一般用戶！` })
+          if (role) {
+            user.update({
+              role: null
             })
-        } else {
-          user.update({
-            role: 'admin'
-          })
-            .then(user => {
-              return callback({ status: 'success', message: `${user.email} 帳號已設定為管理員！` })
+              .then(user => {
+                return callback({ status: 'success', message: `${user.email} 帳號已設定為一般用戶！` })
+              })
+          } else {
+            user.update({
+              role: 'admin'
             })
-        }
-      })
+              .then(user => {
+                return callback({ status: 'success', message: `${user.email} 帳號已設定為管理員！` })
+              })
+          }
+        })
+    }
+    catch (err) {
+      console.log(`Err: ${err}`)
+      return callback({ status: 'error', message: `${user.email} 帳號設定失敗` })
+    }
   },
-
 }
 
 module.exports = adminService  
