af04dc66de1652ec6c28755bf10fe4c0108e7ded
jacs <wbruce0110@gmail.com>
2019-11-10 17:05:53 +0800

    create FB login feature

--- routes/routes.js
+++ routes/routes.js
@@ -10,6 +10,7 @@ const orderRoute = require('./hbs/orderRoute')
 const productRoute = require('./hbs/productRoute')
 const adminRoute = require('./hbs/adminRoute')
 const sendCouponRoute = require('./hbs/sendCouponRoute')
+const authRoute = require('./hbs/authRoute')
 
 router.use('/accounts', accountRoute)
 router.use('/carts', cartRoute)
@@ -17,6 +18,7 @@ router.use('/orders', orderRoute)
 router.use('/products', productRoute)
 router.use('/admin', authenticatedAdmin, adminRoute)
 router.use('/send', sendCouponRoute)
+router.use('/auth', authRoute)
 
 // 將其他 routes 導回首頁
 router.get('/', (req, res) => res.redirect('/products/main'))
