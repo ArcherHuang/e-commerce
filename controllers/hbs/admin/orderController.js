b8138b4226509e419149f148986705a0105862cf
jacs <wbruce0110@gmail.com>
2019-11-12 15:04:06 +0800

    update redirect path of order functions

--- controllers/hbs/admin/orderController.js
+++ controllers/hbs/admin/orderController.js
@@ -49,25 +49,25 @@ const orderController = {
 
   cancelOrder: (req, res) => {
     orderService.cancelOrder(req, res, (data) => {
-      orderController.responseMessageAction(req, res, data, '/admin/orders', 'back')
+      orderController.responseMessageAction(req, res, data, 'back', 'back')
     })
   },
 
   resumeOrder: (req, res) => {
     orderService.resumeOrder(req, res, (data) => {
-      orderController.responseMessageAction(req, res, data, '/admin/orders', 'back')
+      orderController.responseMessageAction(req, res, data, 'back', 'back')
     })
   },
 
   shippedOrder: (req, res) => {
     orderService.shippedOrder(req, res, (data) => {
-      orderController.responseMessageAction(req, res, data, '/admin/orders', 'back')
+      orderController.responseMessageAction(req, res, data, 'back', 'back')
     })
   },
 
   unshippedOrder: (req, res) => {
     orderService.unshippedOrder(req, res, (data) => {
-      orderController.responseMessageAction(req, res, data, '/admin/orders', 'back')
+      orderController.responseMessageAction(req, res, data, 'back', 'back')
     })
   },
 
