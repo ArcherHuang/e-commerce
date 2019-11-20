6f8d4a612d5a078971160a977f78c15dfc4ab048
ArcherHuang <mmosconii@gmail.com>
2019-11-20 10:22:47 +0800

    bug/not-match-route, fix #326

--- routes/routes.js
+++ routes/routes.js
@@ -4,10 +4,6 @@ const passport = require('../config/passport')
 
 const { ensureAuthenticated, isAuthAdmin, getUser, authenticatedAdmin } = require('../config/auth')
 
-// catch all that caught routes not previously defined
-router.all('/products/main([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-router.all('/products/shop([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-
 const accountRoute = require('./hbs/accountRoute')
 const cartRoute = require('./hbs/cartRoute')
 const orderRoute = require('./hbs/orderRoute')
@@ -26,16 +22,7 @@ router.use('/auth', authRoute)
 
 // 將其他 routes 導回首頁
 router.get('/', (req, res) => res.redirect('/products/main'))
-// router.get('/:params', (req, res) => res.redirect('/products/main'))
-
-// catch all that caught routes not previously defined
-router.get('/accounts([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-router.get('/carts([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-router.get('/orders([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-router.get('/products([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-router.get('/admin([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-router.get('/send([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
-router.get('/auth([\\w:./-~@\!\$]+)', (req, res) => res.redirect('/products/main'))
+router.get('*', (req, res) => res.redirect('/products/main'))
 
 module.exports = router
 
